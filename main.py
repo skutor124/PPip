cc1b7d39bce57c4f2f360b03751aafb674ff9115
skutor124 <54344955+skutor124@users.noreply.github.com>
2021-12-27 15:49:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 import tkinter as tk
 from normgostvrode1 import *
+
 def graphic_interface():
     count=0
     def say_hello():
         print("здарова бандиты")
     def create_newlabel():
         label = tk.Label(win,text='some text').pack()
-    def counter():
-        global count
-        count+=1
-        btn_4['text']=f'counter:{count}'
+    # def counter():
+    #     global count
+    #     count+=1
+    #     btn_4['text']=f'counter:{count}'
     def randomcolor():
         win.config(bg='#0e4c6a')
     def get_entry():
@@ -85,81 +85,21 @@ def graphic_interface():
 
     win.mainloop()
 
+
 def main():
 
     graphic_interface()
 
-    # cryptMode = input("[E]ncrypt | [D]escrypt: ").upper()
     key = 18318279387912387912789378912379821879387978238793278872378329832982398023031
-    print(len(str(key)))
     encript_route = r'1.txt'
     route = r'2.txt'
     decript_route = r'3.txt'
-
-    # шифровка
-    cyphred = []  # тут будет хранится зашифрованный текст
-    gost = Crypt(key, blocks)
-    # try:
-    s = []
-    # Читаем из файла текст и шифруем каждую букву
-    with open(encript_route, 'rb') as file:
-        s = file.read()
-        print(s)
-        print(len(s))
-    b = text_to_blocks(s)
-
-    print(b)
-    print(len(b))
-    for x in b:
-        cyphred.append(gost.encrypt(x))
-    """except: # если не удалось открыть файл, выходим
-        print(f"Не удалось открыть файл {encript_route}")
-        return"""
-    # try:
-    s = blocks_to_text(cyphred)
-    # записываем зашифрованный текст в файл
-    with open(route, 'wb') as file:
-        file.write(s)
-    print("Файл зашифрован")
-    """except:
-        print(f"Не удалось открыть файл {decript_route}")
-        return"""
-    # дешифровка
-    decyphred = []  # тут будет храниться расшифрованный текст
-    gost = Crypt(key, blocks)
-    # try:
-    with open(route, 'rb') as file:
-        s = file.read()
-    b = text_to_blocks(s)
-    for x in b:
-        #  расшифровываем текст из файла и добавляем его в список
-        decyphred.append(gost.decrypt(x))
-    """except:
-        print(f"Не удалось открыть файл {decript_route}")
-        return"""
-    s = blocks_to_text(decyphred)
-    # try:
-    with open(decript_route, 'wb') as file:
-        # объеденяем расшифрованные символы в строку и записываем в файл
-        file.write(s)
-    print("Файл расшифрован")
-    """except:
-        print(f"Не удалось открыть файл {encript_route}")
-        return"""
-
+    encrypt_gost(key, encript_route,route)
+    decrypt_gost(key,route, decript_route )
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
 
 
 

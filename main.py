7fc9549e8d5a1ab7cb01acaaf14c7d1da56c858a
skutor124 <54344955+skutor124@users.noreply.github.com>
2021-12-28 12:58:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from normgostvrode1 import *
 import os
-from hashlib import sha256
+import hashlib
 
 
 
@@ -9,8 +9,6 @@ from hashlib import sha256
 key = 18318279387912387912789378912379821879387978238793278872378329832982398023031
 def graphic_interface():
 
-    # key='dsdsd'
-    # print(key)
     def say_hello():
         print("здарова бандиты")
     def create_newlabel():
@@ -23,13 +21,23 @@ def graphic_interface():
         win.config(bg='#0e4c6a')
     def get_key():
         value=name.get()
+        global key
         if value:
-            global key
+            key=new_key(value)
 
-            # print(value)
-            key=value
+
+
     def del_entry():
         name.delete(0,tk.END)
+    def select_cipher():
+        eee=chipher_val.get()
+        chipher_text.set(f'вы выбрали{eee}')
+        if eee==1:
+            print("gost")
+        elif eee == 2:
+            print("aes")
+        elif eee == 3:
+            print("lol")
 
 
 
@@ -77,9 +85,9 @@ def graphic_interface():
     #     for j in range(4):
     #         tk.Button(win, bg='orange', fg='#097a6a', text=f'Hi: {i}\t{j}').grid(row=i, column=j, stick='we')
 
-    win.grid_columnconfigure(0, minsize=200)
-    win.grid_columnconfigure(1, minsize=150)
-
+    # win.grid_columnconfigure(0, minsize=200)
+    # win.grid_columnconfigure(1, minsize=150)
+####
     name = tk.Entry(win, bg='orange', show='*')
     name.grid(row=0, column=1, stick='we')
 
@@ -93,27 +101,32 @@ def graphic_interface():
     tk.Label(win, text='password:').grid(row=2, column=0, stick='we')
     tk.Entry(win, bg='orange', show='*').grid(row=2, column=1, stick='we') #пароль в звёздочках
 
-
+    chipher_val = tk.IntVar()
+    chipher_text=tk.StringVar()
+    tk.Radiobutton(win, text='гост магма', variable=chipher_val, value=1, command=select_cipher).grid(row=0, column=3,stick='w')
+    tk.Radiobutton(win, text='AES', variable=chipher_val, value=2, command=select_cipher).grid(row=1, column=3, stick='w')
+    tk.Radiobutton(win, text='lol', variable=chipher_val, value=3, command=select_cipher).grid(row=2, column=3, stick='w')
+    tk.Label(win, textvariable=chipher_text, bg='#b49add').grid(row=3, column=3, stick='w')
 
     win.mainloop()
 
+def new_key(password):
+    hash_object = hashlib.sha256(bytes(password, 'utf-8'))
+    hex_dig = hash_object.hexdigest()
+    keys = text_to_blocks(bytes(hex_dig, 'utf-8'), 32)
+    key = keys[0]
+    return key
+
 
 def main():
-    input_ = input('Enter something: ')
-    a=sha256(input_.encode('utf-8')).digest()
-    b=sha256(input_.encode('utf-8')).hexdigest()
-    print(a)
-    print(len(a))
-    print(b)
-    # print(key)
+
 
     encript_route = r'1.txt'
     route = r'2.txt'
     decript_route = r'3.txt'
 
-    # graphic_interface()
-
-    # print(key)
+    graphic_interface()
+    print(key)
     # encrypt_gost(key, encript_route,route)
     # decrypt_gost(key,route, decript_route )
 
